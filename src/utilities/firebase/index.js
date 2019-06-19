040c26d1ae7f8a13a5a383be4faf96a59481426f
Taylor Gorman <me@taylorpatrickgorman.com>
2019-06-18 22:31:45 -0500

    👷‍♂️ adding firebase package!

---
+++ src/utilities/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBLidmgV0SP9OdcYMqksnoOnicumIzKyyw",
+  authDomain: "crapper-keeper.firebaseapp.com",
+  databaseURL: "https://crapper-keeper.firebaseio.com",
+  projectId: "crapper-keeper",
+  storageBucket: "crapper-keeper.appspot.com",
+  messagingSenderId: "574852937709",
+  appId: "1:574852937709:web:5e235901c0ae96a9"
+});
+
+export default firebase;
